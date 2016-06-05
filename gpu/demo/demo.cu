db9289de9e121a7fb7560a922715f612f3d1f61d
Chiang, Yi-Yo@home <b03902082@ntu.edu.tw>
2016-06-05 16:51:52 +0800

    demo

--- gpu/demo/demo.cu
+++ gpu/demo/demo.cu
@@ -2,38 +2,57 @@
 #include <iostream>
 #include <cassert>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
 
+#define TIMER_SET(t0) std::chrono::time_point<std::chrono::steady_clock> t0 = std::chrono::steady_clock::now()
+#define TIMER_DIFF(t0, t1) std::chrono::duration_cast<std::chrono::microseconds> (t1 - t0).count()
+
 __global__
-void kernel1(int * A, int * B, int N) {
+void kernel(float * A, int N) {
   int id = blockIdx.x * blockDim.x + threadIdx.x;
   if (id >= N) return;
-  A[id] += B[id];
+  A[id] = sqrtf( powf(3.1415926f, id) );
 }
 
 int main(int argc, char * argv[]) {
-  const int N = 10000;
-  int * A, * B;
+  const int N = 10000000;
+  float * A;
 
-  // A = new int[N];
-  // B = new int[N];
-  cudaMallocManaged(&A, sizeof(int) * N, cudaMemAttachGlobal);
-  cudaMallocManaged(&B, sizeof(int) * N, cudaMemAttachGlobal);
+  TIMER_SET(t0);
 
-  // Do something with A and B ...
+#ifdef CPU_DEMO
+  A = new float[N];
+#else
+  cudaMallocManaged(&A, sizeof(float) * N, cudaMemAttachGlobal);
+#endif
 
-  // Calculate A = A + B (add two vectors)
-  int dimBlock = 32;
-  int dimGrid = N / 32 + 1;
-  kernel1<<<dimGrid, dimBlock>>>(A, B, N);
+  // Calculate A[i] = sqrt( pow(PI, i) ) (add two vectors)
+#ifdef CPU_DEMO
+  for (int i = 0; i < N; ++i) {
+    A[i] = sqrtf( powf(3.1415926f, i) );
+  }
+#else
+  int dimBlock = 64;
+  int dimGrid = (N + dimBlock - 1) / dimBlock;
+  kernel<<<dimGrid, dimBlock>>>(A, N);
   cudaDeviceSynchronize();
+#endif
 
-  // for (int i = 0; i < N; ++i) {
-  //   A[i] += B[i];
+  // for (int i = 0; i < 5; ++i) {
+  //   cout << ' ' << A[i];
   // }
+  // cout << endl;
 
+  TIMER_SET(t1);
+  cout << "Time (microsecond): " << TIMER_DIFF(t0, t1) << endl;
+
+#ifdef CPU_DEMO
+  delete A;
+#else
   cudaFree(A);
-  cudaFree(B);
+#endif
+
   return 0;
 }
